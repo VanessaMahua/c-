16c47aa083ec4b9fd3f6307dcdaf69af70fc8fad
VanessaMahua <vanessa.macedo@ucsp.edu.pe>
2016-08-29 14:59:15 -0500

    Create clase1.cpp

---
+++ clase1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int f,c ;
+    cout << "Cantidad de grados centigrados" << endl;
+    cin>>c;
+    f=9*c/5+32;
+    cout<<"Grados Farenheit="<<f<<endl;
+    return 0;
+
+}
